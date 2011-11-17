dcf6681d5555fe143e2c1477d6916d36140e763f
Chin Huang <pukkaone@gmail.com>
2011-11-16 20:46:08 -0500

    Add localization to sample Sinatra application

--- ruby/sample-sinatra/app.rb
+++ ruby/sample-sinatra/app.rb
@@ -1,7 +1,10 @@
 $LOAD_PATH << File.expand_path('../core/lib', File.dirname(__FILE__))
 
+require 'i18n'
 require 'mullet/tilt'
 require 'sinatra'
+
+I18n.load_path += Dir.glob(File.join(settings.root, 'locale', '*.{rb,yml}'))
 
 helpers do
   def mullet(*args)
@@ -11,5 +14,5 @@ end
 
 get '/' do
   @title = 'Title'
-  mullet :index, locals: { greeting: 'Hello' }
+  mullet :index, locals: { name: 'Chris' }
 end
